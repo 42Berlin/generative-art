1029e297427a50fa3222f57d7c03e9ca0a6ef23c
deozdemi <denisozdemir@hotmail.com>
2023-01-05 15:00:05 +0100

    Added simplePrompt example

---
+++ simplePrompt.py
@@ -0,0 +1,27 @@
+import io
+import os
+import warnings
+from PIL import Image
+from stability_sdk import client
+import stability_sdk.interfaces.gooseai.generation.generation_pb2 as generation
+import showImage
+
+stability_api = client.StabilityInference(
+    key=os.environ['STABILITY_KEY'],
+    verbose=True,
+)
+answers = stability_api.generate(
+    prompt = "Spaceship painted by Vincent Van Gogh",
+    steps = 50, # defaults to 30 if not specified
+    width = 512,
+    height = 256
+)
+for resp in answers:
+    for artifact in resp.artifacts:
+        if artifact.finish_reason == generation.FILTER:
+            warnings.warn(
+                "Your request activated the API's safety filters and could not be processed."
+                "Please modify the prompt and try again.")
+        if artifact.type == generation.ARTIFACT_IMAGE:
+            img = Image.open(io.BytesIO(artifact.binary))
+            showImage.showFullScreen(img)
